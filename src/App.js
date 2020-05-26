494f397e6d0f2b6cedbc4b0fc6d62d14fc00fde4
jessicatriana <jessica.triana.tx@gmail.com>
2020-05-26 12:47:07 -0500

    Built searchMovies.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import SearchMovies from "./searchMovies";
 
 class App extends React.Component {
   render() {
     return (
       <div className="container">
         <h1 className="title">React Movie Search</h1>
+        <SearchMovies />
       </div>
     );
   }
