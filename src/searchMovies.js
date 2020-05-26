ee6fd2d54717009ec1524c6a37bb198b0c6e36df
jessicatriana <jessica.triana.tx@gmail.com>
2020-05-26 13:07:18 -0500

    Built fetch to movie api

--- src/searchMovies.js
+++ src/searchMovies.js
@@ -1,8 +1,25 @@
 import React from "react";
 
 export default function SearchMovies() {
+  const searchMovies = async (event) => {
+    event.preventDefault();
+    console.log("submitting");
+
+    const query = "Jurassic Park";
+
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=0b3fc42b91303295625171df7628e7ce&language=en-US&query=${query}&page=1&include_adult=false`;
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={searchMovies}>
       <label className="label" htmlFor="query">
         {" "}
         Movie Name
